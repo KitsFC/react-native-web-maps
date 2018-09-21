3c9c58525530dab248d2d71f59b6c4e9572db0a0
Felix <github@kitsilanofc.com>
2018-09-21 10:03:32 -0700

    Enhancements to state and props.
    Added support for props.image, props.anchor, and props.onPress.

--- src/Marker.js
+++ src/Marker.js
@@ -2,14 +2,28 @@ import React, { Component } from 'react';
 import { Marker } from 'react-google-maps';
 
 class MapViewMarker extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      title: props.title,
+      position: { lat: props.coordinate.latitude, lng: props.coordinate.longitude },
+      icon: props.image,
+      onClick: props.onPress,
+      anchorPoint: new google.maps.Point(...props.anchor),
+    };
+  };
+  
   render() {
     return (
       <Marker
-        title={this.props.description ? `${this.props.title}\n${this.props.description}` : this.props.title}
-        position={{ lat: this.props.coordinate.latitude, lng: this.props.coordinate.longitude }}
+        title={this.state.title}
+        position={this.state.position}
+        icon={this.state.icon}
+        onClick={this.state.onClick}
+        defaultOptions={{ anchorPoint: this.state.anchorPoint }}
       />
     );
-  }
+  };
 }
 
 export default MapViewMarker;
